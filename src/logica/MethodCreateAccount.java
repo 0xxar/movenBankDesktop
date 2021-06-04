7ce27d2f8e64dddd6957cf90661a37324427f9b2
Oscar <oscareloy2992@hotmail.com>
2021-06-04 00:37:11 -0500

    Se creo la clase MethodCreacte Account y Conexion

---
+++ src/logica/MethodCreateAccount.java
@@ -0,0 +1,21 @@
+package logica;
+
+import logica.Person.*;
+import interfaces.CreateAccount.*;
+
+public class MethodCreateAccount {
+
+    Person person;
+
+    public MethodCreateAccount(String firtName,String lastName,String birthDay,String gender){
+
+        new Person();
+
+        if(person.getFirts_Name().isEmpty()){
+
+
+        }
+
+
+    }
+}
