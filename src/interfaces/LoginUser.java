6ee6148ae8cf05435e7c1a4eb4554f8931f638ec
Oscar <oscareloy2992@hotmail.com>
2021-06-09 00:56:48 -0500

    Se agregaron imagenes para la interfaz de login

--- src/interfaces/LoginUser.java
+++ src/interfaces/LoginUser.java
@@ -17,54 +17,66 @@ public class LoginUser extends JFrame implements ActionListener {
     public JButton bt_login,bt_createCount;
     public JTextField txt_login;
     public JPasswordField txt_password;
-    public JLabel label_down;
+    public JLabel label_down,label_logo;
+
 
 
     public LoginUser(){
 
         //metodo para que los objetos se puedan mover dentro del JFrame
         setLayout(null);
-        setSize(350,450);
+        setSize(450,650);
         setResizable(false);
         setTitle("Sistema de prestamos");
         setLocationRelativeTo(null);
         getContentPane().setBackground(new Color(15,55,150));
+        //setIconImage(new ImageIcon(getClass().getResource("")).getImage());
 
         //metodo para que la aplicacion no quede ne segundo plano
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        ImageIcon imageIcon = new ImageIcon("src/images/user.png");
+        label_logo = new JLabel(imageIcon);
+        label_logo.setBounds(-25,5,500,260);
+        add(label_logo);
+
         txt_login = new JTextField("");
         txt_login.setBackground(new Color(255,255,255));
-        txt_login.setBounds(60,60,220,40);
+        txt_login.setBounds(110,280,220,50);
         txt_login.setFont(new Font("Andale Mono",1,12));
         txt_login.setHorizontalAlignment(JTextField.CENTER);
         add(txt_login);
 
         txt_password = new JPasswordField("");
         txt_password.setBackground(new Color(255,255,255));
-        txt_password.setBounds(60,110,220,40);
+        txt_password.setBounds(110,360,220,50);
         txt_password.setHorizontalAlignment(JTextField.CENTER);
         add(txt_password);
 
         bt_login = new JButton("Acceder");
-        bt_login.setBounds(120,170,100,50);
+        bt_login.setBounds(180,420,100,50);
         bt_login.setFont(new Font("Andale Mono",1,14));
         bt_login.addActionListener(this);
         add(bt_login);
 
         bt_createCount = new JButton("Registrarse");
-        bt_createCount.setBounds(100,240,140,50);
+        bt_createCount.setBounds(160,490,140,50);
         bt_createCount.setFont(new Font("Andale Mono",1,12));
         bt_createCount.addActionListener(this);
         add(bt_createCount);
 
         label_down = new JLabel("Todos los derechos reservados 2021");
         label_down.setBackground(new Color(222,222,222));
-        label_down.setBounds(60,350,220,40);
+        label_down.setBounds(110,550,220,40);
         label_down.setForeground(new Color(222,222,222));
         label_down.setFont(new Font("Andale Mono",1,12));
         add(label_down);
 
+    }
+    @Override
+    public Image getIconImage(){
+        Image retValue = Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("src/images/bank.png"));
+        return retValue;
     }
 
     public static void main(String[] args) {
